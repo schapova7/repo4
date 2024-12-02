6b4b87b66ac762dcb9a07544d97dba4d193eca22
Katya <katyahonor123@gmail.com>
2024-12-02 23:14:12 +0800

    ex0022

--- Ex002/obj/Debug/net8.0/Ex002.AssemblyInfo.cs
+++ Ex002/obj/Debug/net8.0/Ex002.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex002")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaf90059d76a34cf7122a50e331e3f0d07c8d54f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00eeef66e969e0c8fb204e0417b9955e9a7c9cf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex002")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex002")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
